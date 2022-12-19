c386021f36fd1918754e709a74a058fb4feed2a0
Lohan Pinheiro <o.lohan.yrvine@gmail.com>
2022-12-19 13:16:15 -0300

    adding exports

--- lib/game/map_maker/map_maker.dart
+++ lib/game/map_maker/map_maker.dart
@@ -5,5 +5,7 @@ export 'src/map_maker.dart';
 export 'src/components/wall.dart';
 export 'src/components/table.dart';
 export 'src/components/plant.dart';
+export 'src/components/shelf.dart';
+export 'src/components/chair.dart';
 
 export 'src/abstractions/maker.dart';
